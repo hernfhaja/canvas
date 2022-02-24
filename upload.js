3013b16676bb5aad26537cdbe2c6ac5f07b013b4
hernfhaja@gmail.com <hernfhaja@gmail.com>
2022-02-25 00:25:11 +0700

    add new card

---
+++ upload.js
@@ -0,0 +1,6 @@
+const CLIRNT_ID =
+  "1079548728317-9fcmnof0d04l98uran0dod9jokks4fme.apps.googleusercontent.com";
+const CLIRNT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+
+const REFRESH_TOKEN = "";
